0c8df13c49565b91ec30214ff9c1cce515fafa09
Ernest French <ernestfrench@gmail.com>
2012-08-26 20:15:45 +0800

    remove some stuff

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!P:\proj\tracker\env\Scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'django==1.4.1','django-admin.py'
-__requires__ = 'django==1.4.1'
-import pkg_resources
-pkg_resources.run_script('django==1.4.1', 'django-admin.py')
