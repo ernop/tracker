b9bbc85998ca43a44e111d317e2271e160363b37
Ernie French <ernestfrench@gmail.com>
2014-05-21 08:30:02 +0800

    ordering

--- day/photoutil.py
+++ day/photoutil.py
@@ -115,7 +115,7 @@ def get_next_incoming(exclude=None):
             return img
     #no IMG ones, so return
     exis=Photo.objects.exclude(deleted=True).exclude(id__in=exclude).filter(incoming=True)
-    exis=exis.order_by('-day__date','taken','created','modified','id')
+    exis=exis.order_by('-day__date','-id','taken','created','modified','id')
     ii=0
     found=False
     ct=exis.count()
