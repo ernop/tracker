2f234910ebc5ad4d8265a8a62a67e41ecb53381a
Ernie French <ernestfrench@gmail.com>
2014-06-08 12:11:24 +0800

    safe target name

--- day/photoutil.py
+++ day/photoutil.py
@@ -96,7 +96,8 @@ def getletter():
     return random.choice('a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9'.split())
 
 def get_nonexisting_fp(basedir,basefn):
-    fn=basefn
+    from utils import make_safe_filename
+    fn=make_safe_filename(basefn)
     ii=0
     testfp=os.path.join(basedir,fn)
     while os.path.exists(testfp):
