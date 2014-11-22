ea07f0511c3ffbe7bc67595321310f65eb606997
ernie <ernestfrench@gmail.com>
2014-11-22 14:11:30 +0000

    bump

--- wsgi2.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-sys.path.insert(0, '/home/ernie/tracker/')
-sys.path.insert(0, '/home/ernie/tracker/day/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker.settings")
-os.environ.setdefault("PYTHON_EGG_CACHE", "/home/ernop/eggs/.python-eggs")
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
